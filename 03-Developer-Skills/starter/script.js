b8af8c8ac191037db4661ae88d0b777424c06172
tola <a131090a@gmail.com>
2022-03-31 00:32:19 +0300

    developing stuff

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -38,4 +38,42 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // value: Number(prompt('Degrees celsius:'))
+    value: 10
+  };
+  console.table(measurement); // new command ! - v vide tabl
+  // console.warn
+  //console.error
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+
+//using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemps = temps[i];
+    if (typeof curTemps !== 'number') continue;
+
+    if (curTemps > max) max = curTemps;
+    if (curTemps < min) min = curTemps;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
+
 */
