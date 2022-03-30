1a458db9269e0e648a96c394ea250bc5f4327113
tola <a131090a@gmail.com>
2022-03-30 03:01:31 +0300

    solving problems, ussing research lesson

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,11 +1,41 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const xyzabz = 23;
-const doo = 202 - xyzabz;
+/*
+const temperatures = [3, -2, -6, -1, 'error', 9, 17, 15, 14, 13, 9, 5];
 
-const lolo = function () {
-  if (xyzabz > 20) console.log('hello git bash');
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemps = temps[i];
+    if (typeof curTemps !== 'number') continue;
+
+    if (curTemps > max) max = curTemps;
+    if (curTemps < min) min = curTemps;
+  }
+  console.log(max, min);
+  return max - min;
 };
-lolo();
-console.log('SS'); //FIX
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemps = temps[i];
+    if (typeof curTemps !== 'number') continue;
+
+    if (curTemps > max) max = curTemps;
+    if (curTemps < min) min = curTemps;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+*/
