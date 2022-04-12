b6b321ebbefcf9593f191afbd9c312ab606fcd05
summer13t <a131090a@gmail.com>
2022-04-12 05:27:44 +0300

    GuessMyNumber before mastering doubling_code

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,9 +12,7 @@
 // document.querySelector('.guess').value = 17;
 
 const secretNumber = Math.trunc(Math.random()*20) + 1;
-let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
+let score = 20;let highScore = 0;
 
 
 document.querySelector('.check').addEventListener('click', function() {
@@ -25,6 +23,14 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.message').textContent = '⛔ No number! ';
     } else if(guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('body').style.backgroundColor = 'rgb(70, 170, 92)';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+        if(score > highScore) {
+            highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+        };
+
     } else if(guess < secretNumber) {
         if(score > 1) {
         document.querySelector('.message').textContent = 'too low !'; 
@@ -44,6 +50,18 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
     }
+
+   
 });
 
-console.log(secretNumber);
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    const guess = document.querySelector('.guess').value = '';
+
+});
